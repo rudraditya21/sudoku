7d90a954a50c5f1fc7103dd3307c9ea8a1a51dea
Rudraditya Thakur <rudraditya.thakur21@gmail.com>
2026-08-14 22:51:41 +0530

    added: header, vscode settings, shadcn setup, sudoku component skeleton

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const poppins = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -15,10 +16,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
-      lang="en"
-      className={`${poppins.className} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+      lang="en" className={`${poppins.className} h-full antialiased`}>
+      <body className="min-h-full flex flex-col">
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="flex justify-center items-center py-6 px-8 border-b border-border">
+      <h1>Sudoku</h1>
+    </div>
+  )
+}
